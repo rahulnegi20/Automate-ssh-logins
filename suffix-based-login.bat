42e296ee3890c73a1e86b614986a04289e074f32
rahul negi <36270407+rahulnegi20@users.noreply.github.com>
2022-02-09 20:54:57 +0530

    Update suffix-based-login.bat

--- suffix-based-login.bat
+++ suffix-based-login.bat
@@ -1,5 +1,17 @@
 @echo off
 @REM Enter last three number of your server's IP address only to login.
+
+
+@REM Provide your system's address to the putty.exe here
+set putty="C:\Program Files\PuTTY\putty.exe
+
+@REM Provide your username@address here
+set id=eclairs@192.xxx.xx.174
+
+@REM Provide your password here
+set password=passwrd@123
+
 set /p id="Enter the suffix of IP Address you want to login: "
-"C:\Program Files\PuTTY\putty.exe" -pw passwrd@123 username@192.168.12.%id%
+%putty% -pw %password% %id%
+
 @echo 
