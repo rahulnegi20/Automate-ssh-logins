5144dba2339b3c4d7f8f9dc9bcf5a150798f3e1d
rahul negi <36270407+rahulnegi20@users.noreply.github.com>
2022-02-09 20:53:35 +0530

    Update single-ssh.bat

--- single-ssh.bat
+++ single-ssh.bat
@@ -1,5 +1,16 @@
 @echo off
+
 @REM Use this if you have single server to login only, provide your sysyem address to puty.exe below.
 
-"C:\Program Files\PuTTY\putty.exe" -pw yourpasswrd@123 username@192.168.12.156
+@REM Provide your system's address to the putty.exe here
+set putty="C:\Program Files\PuTTY\putty.exe
+
+@REM Provide your username@address here
+set id=eclairs@192.xxx.xx.174
+
+@REM Provide your password here
+set password=passwrd@123
+
+%putty% -pw %password% %id%
+
 @echo 
