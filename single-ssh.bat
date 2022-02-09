f212d8a723aaec44e5562145406efb07ce33aaf9
rahul negi <36270407+rahulnegi20@users.noreply.github.com>
2022-02-09 20:35:58 +0530

    Create single-ssh.bat

---
+++ single-ssh.bat
@@ -0,0 +1,5 @@
+@echo off
+@REM Use this if you have single server to login only, provide your sysyem address to puty.exe below.
+
+"C:\Program Files\PuTTY\putty.exe" -pw yourpasswrd@123 username@192.168.12.156
+@echo 
